723adbb6837ad968c399fa4806d05e4993fa3943
ssolomon2020 <75937136+ssolomon2020@users.noreply.github.com>
2021-12-25 18:53:31 -0500

    Add files via upload

---
+++ Free_Code_Camp/Charles_Severance/Python_for_Everyone/Exercise 02-02.py
@@ -0,0 +1,17 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Student           : Shawn Solomon                               #
+# Date              : 12/25/2021                                  #
+# Course            : Python For Everyone                         #
+# Instructor        : Chuck Severence                             #
+# Exercise          : #02-02                                      #
+# Video             : https://www.youtube.com/watch?v=t_4DPwsaGDY #
+# Learning Platform : www.freecodecamp.org                        #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Program will start after script begins run.
+
+# Create variable 'nzt' and set it as an input function. Function will run and print the string 'Enter your name: ' and wait for user input to set the variable after pressing enter.
+nzt = input("Enter your name: ")
+# Call print function and print the string 'Hello' as well as the variable 'nzt' into the CLI buffer.
+print("Hello", nzt)
+# Program will end and return to the normal CLI cursor.
